ea1ed499ce5ba479e8c2f78d9c3de164a0db8f0a
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2025-04-28 21:00:27 +0530

    server setup

---
+++ app.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.status(200).send("App is working!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
